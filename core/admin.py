688d8ff1d9901bc05dd5fa89c050721b251255e0
Moustapha Diarra <diarra.msa@gmail.com>
2024-01-19 17:11:06 +0300

    Build a models and Set Admin pannels

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import *
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['logo', 'name', 'url']
+    prepopulated_fields = {'slug': ('name', )}
+
+    def logo(self, obj):
+        if obj.logo:
+            return format_html(
+                "<img src='%s' />" % obj.logo.url
+            )
+        return '-'
+    logo.short_description = "Logo"
+
+
+@admin.register(WorkCategory)
+class WorkCategAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'client', 'created']
+    prepopulated_fields = {'slug': ('name', )}
+    list_filter = ['created']
+
+
+@admin.register(TechTool)
+class TechAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- core/models.py
+++ core/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class WorkCategory(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+
+class TechTool(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Work(models.Model):
+    category = models.ForeignKey(WorkCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField()
+    overview = models.CharField(max_length=300)
+    thumbnail = models.ImageField(upload_to='Works/')
+    description = models.TextField()
+    created = models.DateField(auto_now_add=True)
+    client = models.ForeignKey('Client', on_delete=models.CASCADE)
+    usse_tech = models.ForeignKey(TechTool, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    logo = models.ImageField(upload_to='Clients/')
+    slug = models.SlugField()
+    about_firm = models.TextField()
+
+    def __str__(self):
+        return self.name
